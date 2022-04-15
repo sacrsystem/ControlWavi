8afed5f6858ad123fdbc9507352996290a8c49b3
Miriam 8A <amkerry000@gmail.com>
2022-04-15 17:51:20 -0500

    first commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,7 @@ namespace ControlWavi.Controllers
         {
             return View();
         }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
