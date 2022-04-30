cd13b296a3ab7a56e53c8a6495dca03b3f4385d6
Miriam 8A <amkerry000@gmail.com>
2022-04-29 22:43:30 -0500

    correcciones login

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -19,7 +19,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 
-namespace appcomics.Areas.Identity.Pages.Account
+namespace ControlWavi.Areas.Identity.Pages.Account
 {
     public class RegisterModel : PageModel
     {
